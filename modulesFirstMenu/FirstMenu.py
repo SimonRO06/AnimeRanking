46454ff8a2f456d38c1b99b7b00f6b0fd9bb0d9b
simon-rubiano-ortiz <simon.rubiano.ortiz@gmail.com>
2025-03-06 17:42:55 -0500

    feat (FirstMenu): Se creo una carpeta llamada 'modulesFirstMenu' y se creo 'FirstMenu' dentro de esta carpeta, contiene la funcion 'main_menu'

---
+++ modulesFirstMenu/FirstMenu.py
@@ -0,0 +1,28 @@
+from modules.Interfaces import MainMenu
+from modules.ScreenController import limpiar,pausar
+
+def main_menu():
+    while True:
+        limpiar()
+        print(MainMenu)
+        try:
+            seleccion = int(input('> '))
+        except ValueError:
+            print('No es una opcion valida, porfavor escoja una de las opciones...')
+        else:
+            if seleccion == 1:
+                limpiar()
+                pass
+            if seleccion == 2:
+                limpiar()
+                pass
+            if seleccion == 3:
+                limpiar()
+                pass
+            if seleccion == 4:
+                limpiar()
+                print('Programa cerrado...')
+                break
+            else:
+                print('Esta no es una opcion valida...')
+
