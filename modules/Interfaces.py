287a7b985eb2e3c74903ff34fbd74df5a070924d
simon-rubiano-ortiz <simon.rubiano.ortiz@gmail.com>
2025-03-06 17:25:25 -0500

    feat (main): Se añadio un archivo llamado 'Interfaces' a la carpeta 'modules', contiene el diseño de los menus

---
+++ modules/Interfaces.py
@@ -0,0 +1,24 @@
+MainMenu = """
+**************************
+***** MENU PRINCIPAL *****
+**************************
+--------------------------
+1. REGISTRARSE
+2. INICIAR SESION
+3. ELIMINAR CUENTA
+4. SALIR
+--------------------------
+"""
+
+FunctionsMenu = """
+**************************
+***** MENU FUNCIONES *****
+**************************
+--------------------------
+1. REGISTRAE ANIMES
+2. VER RANK GENERAL
+3. VER RANKING PERSONAL
+4. EDITAR ANIMES
+5. REGRESAR
+--------------------------
+"""
